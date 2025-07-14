662736036caa5195d38e550a603d95b70a7f68a5
labibkuy <labibalmubarok12@gmail.com>
2025-07-14 11:38:24 +0700

    Delete coffee-website directory

--- coffee-website/src/scripts/main.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const purchaseButton = document.getElementById('purchase-button');
-
-    purchaseButton.addEventListener('click', function() {
-        window.location.href = 'https://vt.tokopedia.com/t/ZSBqn67ub/';
-    });
-});
